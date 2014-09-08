718d52c556cf631b49f158154955e99128fa0414
Andrew Varvel <andy@fastmail.com.au>
2014-09-08 22:17:25 +1000

    Initial Commit

---
+++ Profile Picture Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile Picture Example
+//
+//  Created by ANDREW VARVEL on 8/09/2014.
+//  Copyright (c) 2014 Andrew Varvel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
