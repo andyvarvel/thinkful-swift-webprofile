ffe8b0a4cbb81361fe52d951476f18883683085a
Andrew Varvel <andy@fastmail.com.au>
2014-09-08 22:23:28 +1000

    Initial commit

--- Profile Picture Example/ViewController.swift
+++ Profile Picture Example/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.profilePicture.alpha = 0.0 //Hide imageView until we get the data
+        
+        let myProfilePictureURL = NSURL(string: "http://graph.facebook.com/andrewv/picture?type=square&width=250&height=250")
+        
+        let activityIndictorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        activityIndictorView.center = self.view.center
+        self.view.addSubview(activityIndictorView)
+        activityIndictorView.startAnimating()
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            // Do some async stuff
+            let imageData = NSData(contentsOfURL: myProfilePictureURL)
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                // Do some main thread stuff
+                self.profilePicture.image = UIImage(data: imageData)
+                activityIndictorView.stopAnimating()
+                
+                UIView.animateWithDuration(1.0, animations: {
+                    self.profilePicture.alpha = 1.0
+                    }, completion: {
+                        (value: Bool) in
+                        println("Animation complete!")
+                })
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
